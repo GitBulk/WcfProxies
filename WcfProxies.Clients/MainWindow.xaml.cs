4ccf62b497fb391248e5b1726562660e8a1be9a6
gitbulk <giacatiensinh@hotmail.com>
2016-07-19 17:34:40 +0700

    create client to test wcf service

---
+++ WcfProxies.Clients/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WcfProxies.Contracts.Data;
+using WcfProxies.Proxies.Shared;
+
+namespace WcfProxies.Clients
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSharedClient_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //BlogPostClient sharedProxy = new BlogPostClient("tcp");
+                EndpointAddress address = new EndpointAddress("http://localhost:9002/BlogPostService");
+                System.ServiceModel.Channels.Binding binding = new WSHttpBinding();
+                BlogPostClient sharedProxy = new BlogPostClient(binding, address);
+                PostData firstPost = sharedProxy.GetPost(1);
+                if (firstPost != null)
+                {
+                    lbxResult.Items.Clear();
+                    lbxResult.Items.Add(firstPost.Title + " by " + firstPost.Author);
+                }
+                sharedProxy.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnNonSharedClient_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnNonSharedClientInjected_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WcfProxies.Clients/readme.txt
@@ -0,0 +1,6 @@
+﻿Add references:
+WcfProxies.Data, WcfProxies.Contracts and WcfProxies.Proxies
+
+Add 2 assemblies
+-System.ServiceModel
+-System.Runtime.Serialization
